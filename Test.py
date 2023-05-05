923fc8b73663595871dad922ea4e96d2b7466b5e
Julio-Cesar-Code <julio111499@gmail.com>
2023-05-05 15:54:00 -0500

    general correction

--- Test.py
+++ Test.py
@@ -3,7 +3,6 @@ from docx import Document
 from docx.shared import Inches
 
 # Leer los datos de Excel con pandas
-pastYear = pd.read_csv('Past.csv', sep=',')
-nextYear=pd.read_csv('Programming.csv', sep=',')
-
-print (nextYear.iloc[0,9])
+pastYear = pd.read_csv('pastYearCamarena.csv', sep=',')
+nextYear=pd.read_csv('nextYearCamarena.csv', sep=',')
+print (nextYear.head())
